f99f70b3ea1eb4f8e6e42faa9ef223e1241398ad
OrzXdOne <johnnytank@gmail.com>
2026-09-12 16:13:10 +0800

    Create hs_snippets.lua

---
+++ hs_snippets.lua
@@ -0,0 +1,29 @@
+-- ~/.hammerspoon/hs_snippets.lua
+-- 本檔案由 Python 腳本自動生成，轉換自 Raycast Snippets 🌟
+return {
+    [";link"]    = [==[[點此造訪]($CLIPBOARD$)]==],
+    [";href"]    = [==[<a href="$CURSOR$">$CLIPBOARD$</a>]==],
+    [":now"]     = [==[{datetime}]==],
+    [";check"]   = [==[✓]==],
+    [";cross"]   = [==[✗]==],
+    ["-> "]      = [==[→]==],
+    ["<- "]      = [==[←]==],
+    [";star"]    = [==[⭐️]==],
+    [";tip"]     = [==[💡]==],
+    [";lol"]     = [==[😂]==],
+    [";good"]    = [==[👍]==],
+    [";date"]    = [==[{date format="yyyy-MM-dd"}]==],
+    [";time"]    = [==[{time format="HH%%:mm"}]==],
+    [";hsshow"]  = [==[hs.alert.show("直接打字即時觸發版已就緒 🚀")]==],
+    [";reply"]   = [==[親愛的客戶您好：
+
+感謝您的來信，有關您詢問的部分：
+
+$CURSOR$
+
+祝您順心，謝謝！]==],
+    [";sign"]    = [==[Best regards,
+
+Johnny Tan
+Email: johnnytank@gmail.com $CURSOR$]==],
+}
