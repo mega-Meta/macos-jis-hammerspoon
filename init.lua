302b7bd927f93fb69fbb2c8c96ed2620871de797
OrzXdOne <johnnytank@gmail.com>
2026-09-12 16:13:00 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,17 @@
 -- init.lua
 -- For JIS keyboard IME Switcher & Shottr Premium Full Integration
 
+--[[
+-- 載入名為 WindowHalfsies 的 Spoon
+hs.loadSpoon("WindowHalfsies")
+]]--
+require("snippets")
+require("auto_reload")
+--require("hs_debug") -- show keyid & appsid on console
+require("spoon_clipboardtool")
+--require("spoon_windowsigils")
+require("spoon_brewinfo")
+
 local DEBUG_FLAG = false  --off is false
 local EISUU_KEY = 102
 local KANA_KEY = 104
@@ -363,35 +374,21 @@ secureInputTimer = hs.timer.doEvery(3, function()
 end)
 :start()
 
--- below function show info. when debug_flag is ON
--- Create a key logger to show keycodes and modifiers
-keyLogger = hs.eventtap
-   .new({ hs.eventtap.event.types.keyDown }, function(event)
-	local keyCode = event:getKeyCode()
-	local flags = event:getFlags()
+-- 	⭐️ 穩定的寫法：加入唯一的識別名稱，讓 macOS 記住它的位置
+--myMenu = hs.menubar.new(true, "myUniqueHammerspoonMenuName")
 
-	-- Format and print the key details to the Hammerspoon Console
-	if DEBUG_FLAG then
-	   print(string.format("Key Code: %s | Modifiers: %s", keyCode, hs.inspect(flags)))
-    end
-	return false -- Return false so the key press still reaches the OS/apps
-end)
-:start()
-
+-- 💡 改良版：延遲 0.2 秒再重載，防止選單列圖示變成空白
 --[[
- --Show application system ID on console when Debug_flag is ON
- 1.點擊畫面右上角選單列的 Hammerspoon 圖示，選擇 「Open Console」（打開主控台）。
- 2.在主控台最下方的輸入框中，複製並貼上以下這行程式碼，然後按下 Enter 鍵執行：
- testWatcher = hs.application.watcher.new(function(name, event, app) if event == hs.application.watcher.activated and app then print("App名稱: " .. name .. " | BundleID: " .. tostring(app:bundleID())) end end):start()
- 3.執行後，你可以直接用滑鼠去點擊你想查詢的 App（例如微信、Discord 或備忘錄）。這時 Hammerspoon 主控台就會立刻即時印出該 App 的準確 ID，例如：App名稱: 備忘錄 | BundleID: com.apple.NotesApp名稱: WeChat | BundleID: com.tencent.xinWeChat
- 4.關閉偵測：查詢完畢後，在主控台輸入 testWatcher:stop() 即可關閉這個臨時偵測器。
-]]
---
-testWatcher = hs.application.watcher.new(function(name, event, app)
-   if event == hs.application.watcher.activated and app and DEBUG_FLAG then
-      print("App名稱: " .. name .. " | BundleID: " .. tostring(app:bundleID()))
-   end
+hs.hotkey.bind({"cmd", "alt"}, "C", function()
+    --hs.alert.show("正在重載設定...")
+    hs.timer.doAfter(0.2, function()
+        hs.reload()
+    end)
+    
 end)
 :start()
+]]--
+-- 每次載入設定時，自動清空 Console 視窗
+hs.console.clearConsole()
 
 hs.autoLaunch(true)
