17dac1dfa3a4cc5d3e1e8cbe9859ebe862dbe583
OrzXdOne <johnnytank@gmail.com>
2026-09-15 23:07:40 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@
 --require("hs_clip2texteditor") --使用MACOS內建texteditor (使用熱鍵 cmd+opt+K) 已預含於section8不需再載入
 --require("hs_clip2vimr")  --使用需先安裝VIMR (使用熱鍵 cmd+opt+K)
 --require("hs_esp_reload") -- 使用需先安裝espanso,自定snippets
-require("hs_clip2coteditor") --使用需先安裝coteditor(使用熱鍵 cmd+opt+M)
+--require("hs_clip2coteditor") --使用需先安裝coteditor(使用熱鍵 cmd+opt+M)
 
 -- ==============================================================================
 -- 終極完美簡易版 init.lua (第一部分：核心基礎、變數與純手動輸入法切換)
@@ -26,7 +26,7 @@ local LALT_KEY = 58
 local RALT_KEY = 61
 local YEN_KEY = 93
 local RFN_KEY = 179
-local DOUBLE_CLICK_TIMER = 0.28 -- 0.28 秒黃金判定時間，讓所有雙擊操作更輕鬆
+local DOUBLE_CLICK_TIMER = 0.35 -- 0.28 秒黃金判定時間，讓所有雙擊操作更輕鬆
 local ABC_IME_ID = "com.apple.keylayout.ABC"
 local CLICK_IME_ID = "com.apple.inputmethod.TCIM.Cangjie" -- 預設切換倉頡
 --local CLICK_IME_ID = "com.apple.inputmethod.TCIM.Zhuyin" --#繁體倚天注音
@@ -377,7 +377,7 @@ fnTap = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.type
                     hs.timer.doAfter(0.05, function()
                         hs.eventtap.keyStroke({"cmd", "shift"}, "h", 0)
                     end)
-                    hs.alert.show("📺 已自動返回 YouTube 首頁")
+                    --hs.alert.show("📺 已自動返回 YouTube 首頁")
                 end
                 lastFnTime = 0
                 return true
@@ -409,12 +409,14 @@ arrowTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
             
         -- fn + 向右鍵 (Keycode 124) -> 映射為小寫 j (YouTube 原生下一部影片)
         elseif keyCode == 124 then
-            hs.eventtap.keyStroke({}, "j", 0)
+            --hs.eventtap.keyStroke({}, "j", 0)
+            hs.eventtap.keyStroke({}, "f", 0)
             return true
             
         -- fn + 向左鍵 (Keycode 123) -> 映射為小寫 k (YouTube 原生上一部影片)
         elseif keyCode == 123 then
-            hs.eventtap.keyStroke({}, "k", 0)
+            --hs.eventtap.keyStroke({}, "k", 0)
+            hs.eventtap.keyStroke({}, "escape", 0)
             return true
         end
     end
